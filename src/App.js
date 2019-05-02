e095fc15014abb6b14c226d5362f69fb3cabbecc
Gosia <mkrzek@googlemail.com>
2019-05-02 10:53:59 +0100

    feat: add dynamic loading

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {hot} from 'react-hot-loader'
 import './styles.css'
+const Warning=React.lazy(()=>import('./Warning'))
 class App extends React.Component{
   state={
     count:0
@@ -20,6 +21,12 @@ class App extends React.Component{
     <h2 className={count > 10 ? 'warning' : null}>Count: {count}</h2>
     <button onClick={()=>{this.increment()}}>+</button>
     <button onClick={()=>{this.decrement()}}>-</button>
+    {count>10 ? 
+    <React.Suspense fallback={null}>
+      <Warning/>
+    </React.Suspense>
+     : null}
+
     </div>
   )
   }
