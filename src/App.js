aee09d7c430cefd2533e723e343277eeed23ce32
Gosia <mkrzek@googlemail.com>
2019-05-07 10:55:21 +0100

    feat: add prop-types and error boundary

--- src/App.js
+++ src/App.js
@@ -6,15 +6,16 @@ class App extends React.Component {
   state = {
     count: 0
   }
+
   increment = () => {
     this.setState(state => ({ count: state.count + 1 }))
   }
   decrement = () => {
     this.setState(state => ({ count: state.count - 1 }))
   }
+
   render() {
     const { count } = this.state
-
     return (
       <div>
         <h1>Hello </h1>
